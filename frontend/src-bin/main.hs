be437074a73d8a20b1d0303aa1b6f53a8b98bb30
AHaliq <liqo12@hotmail.com>
2021-09-21 16:07:58 +0800

    add process

--- frontend/src-bin/main.hs
+++ frontend/src-bin/main.hs
@@ -4,7 +4,10 @@ import Obelisk.Frontend
 import Obelisk.Route.Frontend
 import Reflex.Dom
 
+import Lib (somePrc)
+
 main :: IO ()
 main = do
   let Right validFullEncoder = checkEncoder fullRouteEncoder
+  somePrc (static @"z3.wasm/z3w.wasm")
   run $ runFrontend validFullEncoder frontend
--- z3test/src/Lib.hs
+++ z3test/src/Lib.hs
@@ -1,10 +1,19 @@
 module Lib
     ( someFunc
     , someStr
+    , somePrc
     ) where
+
+import System.Process (runInteractiveProcess)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 someStr :: String
 someStr = "hello from z3 test"
+
+somePrc :: String -> IO ()
+somePrc x = do
+    runInteractiveProcess x [] Nothing Nothing
+    pure ()
+
