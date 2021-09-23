e34b166fa6aeed386ccb2711a1bd660565b0fc5d
AHaliq <liqo12@hotmail.com>
2021-09-23 11:48:22 +0800

    manual insert nix

--- default.nix
+++ default.nix
@@ -15,9 +15,40 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ pkgs, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+  overrides = self: super: let
+    gf-udSrc = pkgs.fetchFromGitHub {
+      owner = "GrammaticalFramework";
+      repo = "gf-ud";
+      rev = "4b0760e02f9efdb8fbdd47ed2258e9b89f15a14d";
+      sha256 = "12y94z3ibrrx0ylwr66i3ibrraj2bzjq97gcnqv71k40z56zk42h";
+    };
+  in
+  {
+    gf-ud = self.callCabal2nix "gf-ud" gf-udSrc {};
+    lsp = self.callHackageDirect {
+      pkg = "lsp";
+      ver = "1.1.1.0";
+      sha256 = "sha256:0lcqiw5304llxamizza28xy4llhmmrr3dkvlm4pgrhzfcxqwnfrm";
+    } {};
+    lsp-types = self.callHackageDirect {
+      pkg = "lsp-types";
+      ver = "1.1.0.0";
+      sha256 = "sha256:1l8g7iq9zsq19hxamy37hf61bmld500pha2xcwwqs7hk53k9wgn8";
+    } {};
+    lsp-test = self.callHackageDirect {
+      pkg = "lsp-test";
+      ver = "0.13.0.0";
+      sha256 = "sha256:1b0p678bh4h1mfbi1v12g9zhnyhgq5q3fiv491ni461v44ypr6bn";
+    } {};
+    sbv = self.callHackageDirect {
+      pkg = "sbv";
+      ver = "8.9";
+      sha256 = "sha256:1bmvibjwdn9pbpmslbjx4jlki9djyv2wa8gm33wxfvvrp3fv6hci";
+    } {};
+  };
 })
