8fcfbfc9c3df4d09034ef53e0fa1ed41768da5d5
AHaliq <liqo12@hotmail.com>
2021-09-27 17:18:47 +0800

    newline on commas

--- frontend/src/StringUtils.hs
+++ frontend/src/StringUtils.hs
@@ -5,15 +5,18 @@ module StringUtils
   ) where
 
 import Data.List (intercalate)
+import Data.Char (isSpace)
 
 indent :: String -> String
 indent [] = []
-indent xs = intercalate "\n" $ map (\(s, i) -> replicate (i*2) ' ' ++ s ) $ breaki 0 xs
+indent xs = intercalate "\n" $ map (\(s, i) -> replicate (i*2) ' ' ++ s ) $ map (\(s,i) -> (dropWhile isSpace s, i)) $ breaki 0 xs
 
 breaki :: Int -> String -> [(String, Int)]
 breaki i [] = [([], i)]
 breaki i (x:xs) = 
-  if x `elem` ['(', '[', '{'] then
+  if x `elem` [','] then
+    (x:"", i):breaki i xs
+  else if x `elem` ['(', '[', '{'] then
     (x:"", i):breaki (i + 1) xs
   else if x `elem` [')', ']', '}'] then
     case breaki (i - 1) xs of
