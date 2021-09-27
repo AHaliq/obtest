a9908a8f6a2f111032b9707d9e652741bd55c093
AHaliq <liqo12@hotmail.com>
2021-09-27 16:35:52 +0800

    reformat lines for breaki function

--- frontend/src/StringUtils.hs
+++ frontend/src/StringUtils.hs
@@ -13,10 +13,13 @@ indent xs = intercalate "\n" $ map (\(s, i) -> replicate (i*2) ' ' ++ s ) $ brea
 breaki :: Int -> String -> [(String, Int)]
 breaki i [] = [([], i)]
 breaki i (x:xs) = 
-  if x `elem` ['(', '[', '{']         then (x:"", i):breaki (i + 1) xs
-  else if x `elem` [')', ']', '}']    then case breaki (i - 1) xs of
-                                        (str, i'):rs -> ("", i):(x:str, i'):rs
-                                        _ -> error "invalid breaki on dedent"
-                                      else case breaki i xs of
-                                        (str, i'):rs -> (x:str, i'):rs
-                                        _ -> error "invalid breaki on same indent"
+  if x `elem` ['(', '[', '{'] then
+    (x:"", i):breaki (i + 1) xs
+  else if x `elem` [')', ']', '}'] then
+    case breaki (i - 1) xs of
+      (str, i'):rs -> ("", i):(x:str, i'):rs
+      _ -> error "invalid breaki on dedent"
+  else
+    case breaki i xs of
+      (str, i'):rs -> (x:str, i'):rs
+      _ -> error "invalid breaki on same indent"
